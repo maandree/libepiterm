c8f7d123038ddc8fc5659fbe6be104fc357e8b85
Mattias Andrée <maandree@operamail.com>
2015-09-05 17:19:23 +0200

    almost there
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/test.c
+++ src/test.c
@@ -1,5 +1,5 @@
 #define _XOPEN_SOURCE  700
-#define _GCC_SOURCE
+#define _GNU_SOURCE
 #define _DEFAULT_SOURCE
 #include <stdlib.h>
 #include <signal.h>
@@ -9,15 +9,31 @@
 #include <termios.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <sys/select.h>
+#include <inttypes.h>
+#include <string.h>
+#include <errno.h>
+
+
+
+#define t(...)  if (called = #__VA_ARGS__, __VA_ARGS__) goto fail
+static const char* called = NULL;
 
 
 
 static volatile sig_atomic_t winched = 0;
+static volatile sig_atomic_t chlded = 0;
 
 
 static void sigwinch(int signo)
 {
   winched = 1;
+  (void) signo;
+}
+
+static void sigchld(int signo)
+{
+  chlded = 1;
   (void) signo;
 }
 
@@ -32,7 +48,7 @@ static void copy_winsize(int whither, int whence)
 
 int main(void)
 {
-  int master, slave, dups;
+  int master, slave, sttyed = 0, r;
   const char* slavename;
   struct sigaction action;
   sigset_t sigset;
@@ -41,6 +57,9 @@ int main(void)
   struct passwd* pw;
   struct termios termios;
   struct termios saved_termios;
+  fd_set fds;
+  char buffer[1024];
+  ssize_t got;
   
   shell = getenv("SHELL");
   if ((shell == NULL) || (access(shell, X_OK) < 0))
@@ -52,31 +71,37 @@ int main(void)
   if (shell == NULL)
     shell = "/bin/sh";
   
-  master = posix_openpt(O_RDWR | O_NOCTTY);
-  grantpt(master);
-  unlockpt(master);
-  slavename = ptsname(master);
-  slave = open(slavename, O_RDWR | O_NOCTTY);
+  t (master = posix_openpt(O_RDWR | O_NOCTTY), master < 0);
+  t (grantpt(master));
+  t (unlockpt(master));
+  t (slavename = ptsname(master), slavename == NULL);
+  t (slave = open(slavename, O_RDONLY | O_NOCTTY), slave < 0);
+  
+  t (tcgetattr(STDIN_FILENO, &termios));
+  t (tcsetattr(master, TCSANOW, &termios));
+  copy_winsize(master, STDIN_FILENO);
+  saved_termios = termios;
+  cfmakeraw(&termios);
+  t (tcsetattr(STDIN_FILENO, TCSANOW, &termios));
+  sttyed = 1;
   
   sigemptyset(&sigset);
   action.sa_handler = sigwinch;
   action.sa_mask = sigset;
   action.sa_flags = 0;
+  t (sigaction(SIGWINCH, &action, NULL));
   
-  tcgetattr(STDIN_FILENO, &termios);
-  tcsetattr(master, TCSANOW, &termios);
-  copy_winsize(master, STDIN_FILENO);
-  saved_termios = termios;
-  cfmakeraw(&termios);
-  tcsetattr(STDIN_FILENO, TCSANOW, &termios);
+  sigemptyset(&sigset);
+  action.sa_handler = sigchld;
+  action.sa_mask = sigset;
+  action.sa_flags = 0;
+  t (sigaction(SIGCHLD, &action, NULL));
   
-  sigaction(SIGWINCH, &action, NULL);
-  
-  pid = fork();
+  t (pid = fork(), pid < 0);
   if (pid == 0)
     goto child;
   
-  for (;;)
+  while (!chlded)
     {
       if (winched)
 	{
@@ -84,23 +109,65 @@ int main(void)
 	  copy_winsize(master, STDIN_FILENO);
 	}
       
-      /* SELECT AND SPLICE  */
+      FD_ZERO(&fds);
+      FD_SET(master, &fds);
+      FD_SET(STDIN_FILENO, &fds);
+      
+      t (r = select(2, &fds, NULL, NULL, NULL), r < 0);
+      
+      if (FD_ISSET(master, &fds))
+	{
+	  t (got = read(master, buffer, sizeof(buffer)), got <= 0);
+	  t (write(STDOUT_FILENO, buffer, (size_t)got) < 0);
+	}
+      if (FD_ISSET(STDIN_FILENO, &fds))
+	{
+	  t (got = read(STDIN_FILENO, buffer, sizeof(buffer)), got <= 0);
+	  t (write(master, buffer, (size_t)got) < 0);
+	}
     }
   
-  tcsetattr(STDIN_FILENO, TCSADRAIN, &saved_termios);
-  close(master);
+  t (tcsetattr(STDIN_FILENO, TCSADRAIN, &saved_termios));
+  t (close(master));
   return 0;
   
+#undef t
+#define t(...)  if (called = #__VA_ARGS__, __VA_ARGS__) goto cfail
  child:
-  close(master);
-  dups = 0;
-  if (slave != STDIN_FILENO)   dups++, dup2(slave, STDIN_FILENO);
-  if (slave != STDOUT_FILENO)  dups++, dup2(slave, STDOUT_FILENO);
-  if (slave != STDERR_FILENO)  dups++, dup2(slave, STDERR_FILENO);
-  if (dups == 3)
-    close(slave);
+  for (r = 0; r < 1024; r++)
+    if ((r != slave) && (r != 64))
+      close(r);
+  if (slave != 0)
+    {
+      t (dup2(slave, STDIN_FILENO));
+      t (close(slave));
+    }
+  t (r = open(slavename, O_WRONLY), r < 0);
+  if (r != STDOUT_FILENO)
+    {
+      t (dup2(r, STDOUT_FILENO));
+      t (close(r));
+    }
+  t (r = open(slavename, O_WRONLY), r < 0);
+  if (r != STDERR_FILENO)
+    {
+      t (dup2(r, STDERR_FILENO));
+      t (close(r));
+    }
+  /* t (setsid() < 0);                      */
+  /* t (ioctl(STDIN_FILENO, TIOCSCTTY, 0)); */
+  close(2);
+  dup2(64, 2);
+  close(64);
   execl(shell, shell, NULL);
-  perror(shell);
+ cfail:
+  dprintf(64, "%s: %s: %s\r\n", shell, called, strerror(errno));
+  return 1;
+  
+ fail:
+  fprintf(stderr, "%s: %s\r\n", called, strerror(errno));
+  if (sttyed)
+    tcsetattr(STDIN_FILENO, TCSADRAIN, &saved_termios);
   return 1;
 }
 
